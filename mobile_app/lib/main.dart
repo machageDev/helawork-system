5bc0107085a7da853aa10d43eb2d098e0c4bef30
machageDev <machagefranklyn@gmail.com>
2025-08-27 23:09:43 +0300

    main maining

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:helawork_flutter/nav/dashboard_screen.dart';
+import 'package:helawork_flutter/screens/forgot_password.dart';
 import 'client.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
+import 'package:helawork_flutter/screens/reset_password.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,10 +19,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'HelaWork',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/login',
+      initialRoute: '/dashboard',
       routes: {
         '/login': (context) => LoginScreen(client: client),
         '/register': (context) => RegisterScreen(client: client),
+        '/forgot_password':(context) => ForgotPasswordScreen(),
+        '/reset_password':(context)=> ResetPasswordScreen(),
+        '/dashboard':(context) => DashboardScreen(),
+        
+
       },
     );
   }
